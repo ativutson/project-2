371accc622ef3fa5917672d36f0184e296320eca
Ativut Son <nazareth_0@hotmail.com>
2021-05-09 09:35:33 -0400

    Commit to Azure

--- config/env/local-production.js
+++ config/env/local-production.js
@@ -1,5 +1,5 @@
 module.exports = {
     db: {
-      uri: 'mongodb://sons-blogs-cosmos:JayPpbfLZ9DT68B5tcw7hbAALbbp0mXkolP59l1ohABGwhjHCtOMsV7HK5A9bKNAWJeGGV7wjf61WLbyWP6jsw==@sons-blogs-cosmos.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false'
+      uri: 'mongodb://sons-blogs-cosmos:JayPpbfLZ9DT68B5tcw7hbAALbbp0mXkolP59l1ohABGwhjHCtOMsV7HK5A9bKNAWJeGGV7wjf61WLbyWP6jsw==@sons-blogs-cosmos.documents.azure.com:10250/?ssl=true&sslverifycertificate=false' || process.env.MONGODB_URI
     }
   };
